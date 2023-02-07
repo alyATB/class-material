8660c2e607ac68403e2736177919bc68bf7b1d12
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2023-02-06 19:24:33 -0800

    DOM

---
+++ 4-DOM/1-events.js
@@ -0,0 +1,58 @@
+function handleClick() {
+  console.log("🐛 IT GOT CLICKED!!!");
+}
+const hooray = () => console.log("HOORAY!");
+
+const butts = document.querySelector(".butts");
+const coolButton = document.querySelector(".cool");
+
+butts.addEventListener("click", handleClick);
+coolButton.addEventListener("click", hooray);
+
+// butts.removeEventListener("click", handleClick);
+
+// ================================================
+
+// function handleBuyButtonClick(e) {
+//   const button = e.target;
+//   console.log(button);
+//   console.log(button.textContent);
+//   // console.log(button.dataset.price);
+//   // console.log(button.dataset.name);
+
+//   // console.log(e.target);
+//   // console.log(e.currentTarget);
+//   // console.log(e.target === e.currentTarget);
+
+//   // e.stopPropagation();
+// }
+// const buyButtons = document.querySelectorAll("button.buy");
+
+// buyButtons.forEach(function (buyButton) {
+//   buyButton.addEventListener("click", handleBuyButtonClick);
+// });
+
+// // ================================================
+
+// const pEl = document.querySelector(".p-1");
+// const div1El = document.querySelector(".div-1");
+// const div2El = document.querySelector(".div-2");
+// const div3El = document.querySelector(".div-3");
+
+// pEl.addEventListener("click", () => console.log("I'm the P"));
+// // div1El.addEventListener("click", () => console.log("I'm the div1El"));
+// // div2El.addEventListener("click", () => console.log("I'm the div2El"));
+// // div3El.addEventListener("click", () => console.log("I'm the div3El"));
+
+
+// // window.addEventListener("click", function () {
+// //   console.log("window clicked");
+// // });
+
+// // ================================================
+
+// const photoEl = document.querySelector(".photo");
+
+// photoEl.addEventListener("mouseenter", (e) => {
+//   console.log(e.currentTarget);
+// });
---
+++ 4-DOM/todoList.js
@@ -0,0 +1,2 @@
+const todoForm = document.querySelector(".todo-form");
+const list = document.querySelector(".list");
