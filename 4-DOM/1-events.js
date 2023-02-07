1300541cf354f067d417f856b29c64f6c3ee1285
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2023-02-06 21:14:46 -0800

    TODO items

--- 4-DOM/1-events.js
+++ 4-DOM/1-events.js
@@ -9,50 +9,52 @@ const coolButton = document.querySelector(".cool");
 butts.addEventListener("click", handleClick);
 coolButton.addEventListener("click", hooray);
 
-// butts.removeEventListener("click", handleClick);
+butts.removeEventListener("click", handleClick);
 
 // ================================================
 
-// function handleBuyButtonClick(e) {
-//   const button = e.target;
-//   console.log(button);
-//   console.log(button.textContent);
-//   // console.log(button.dataset.price);
-//   // console.log(button.dataset.name);
+function handleBuyButtonClick(e) {
+  // const button = e.target;
+  // console.log(button);
+  // console.log(button.textContent);
+  // console.log(button.dataset.price);
+  // console.log(button.dataset.name);
 
-//   // console.log(e.target);
-//   // console.log(e.currentTarget);
-//   // console.log(e.target === e.currentTarget);
+  console.log(e.target);
+  console.log(e.currentTarget);
+  // console.log(e.target === e.currentTarget);
 
-//   // e.stopPropagation();
-// }
-// const buyButtons = document.querySelectorAll("button.buy");
+  // e.stopPropagation();
+}
+const buyButtons = document.querySelectorAll("button.buy");
 
-// buyButtons.forEach(function (buyButton) {
-//   buyButton.addEventListener("click", handleBuyButtonClick);
-// });
-
-// // ================================================
-
-// const pEl = document.querySelector(".p-1");
-// const div1El = document.querySelector(".div-1");
-// const div2El = document.querySelector(".div-2");
-// const div3El = document.querySelector(".div-3");
-
-// pEl.addEventListener("click", () => console.log("I'm the P"));
-// // div1El.addEventListener("click", () => console.log("I'm the div1El"));
-// // div2El.addEventListener("click", () => console.log("I'm the div2El"));
-// // div3El.addEventListener("click", () => console.log("I'm the div3El"));
-
-
-// // window.addEventListener("click", function () {
-// //   console.log("window clicked");
-// // });
+buyButtons.forEach(function (buyButton) {
+  buyButton.addEventListener("click", handleBuyButtonClick);
+});
 
 // // ================================================
 
-// const photoEl = document.querySelector(".photo");
+const pEl = document.querySelector(".p-1");
+const div1El = document.querySelector(".div-1");
+const div2El = document.querySelector(".div-2");
+const div3El = document.querySelector(".div-3");
 
-// photoEl.addEventListener("mouseenter", (e) => {
-//   console.log(e.currentTarget);
-// });
+pEl.addEventListener("click", (e) => {
+  console.log("from the P element");
+  e.stopPropagation();
+});
+div1El.addEventListener("click", () => console.log("I'm the div1El"));
+div2El.addEventListener("click", () => console.log("I'm the div2El"));
+div3El.addEventListener("click", () => console.log("I'm the div3El"));
+
+window.addEventListener("click", function () {
+  console.log("window clicked");
+});
+
+// // ================================================
+
+const photoEl = document.querySelector(".photo");
+
+photoEl.addEventListener("mouseenter", (e) => {
+  console.log(e.currentTarget);
+});
--- 4-DOM/_sol_todoList.js
+++ 4-DOM/_sol_todoList.js
@@ -1,28 +1,53 @@
 const todoForm = document.querySelector(".todo-form");
 const list = document.querySelector(".list");
+
+// We need an array to hold our state
 let items = [];
 
 function updateItems() {
-  // list.innerHTML = "<li>anything</li> <li>anything 2</li> <li>anything 3</li>"
-
   const html = items
     .map(
       (item) =>
         `<li class="todo-item">
-            <input value="${item.id}" type="checkbox" 
-              ${item.completed ? "checked" : ""}  
-            />
-            <span class="itemName">${item.name}</span>
-            <button class="remove-item" value="${item.id}"> X </button>
-         </li>`
+          <input value="${item.id}" type="checkbox" 
+            ${item.completed ? "checked" : ""}  
+          />
+          <span class="itemName">${item.name}</span>
+          <button class="remove-item" value="${item.id}"> X </button>
+      </li>`
     )
     .join("");
-
-  // console.log(html);
 
   list.innerHTML = html;
 
   saveToLocalStorage();
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const name = e.currentTarget.item.value;
+
+  if (!name) return;
+
+  const item = {
+    id: Date.now(), // one quick way to generate a random unique id
+    name: name,
+    completed: false,
+  };
+
+  items.push(item);
+
+  console.log(`There are now ${items.length} in your state`);
+
+  e.target.reset();
+
+  updateItems();
+}
+
+function saveToLocalStorage() {
+  console.info("Saving items to localstorage");
+  localStorage.setItem("items", JSON.stringify(items));
 }
 
 function deleteItem(id) {
@@ -36,10 +61,10 @@ function toogleItem(id) {
   updateItems();
 }
 
-list.addEventListener("click", function (e) {
-  console.log(e.target)
-  const id = parseInt(e.target.value);
+todoForm.addEventListener("submit", handleSubmit);
 
+list.addEventListener("click", function (e) {
+  const id = parseInt(e.target.value);
   if (e.target.matches("button")) {
     deleteItem(id);
   } else if (e.target.matches('input[type="checkbox"]')) {
@@ -47,45 +72,14 @@ list.addEventListener("click", function (e) {
   }
 });
 
-function saveToLocalStorage() {
-  console.info("Saving items to localstorage");
-  localStorage.setItem("items", JSON.stringify(items));
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  console.log("submit");
-
-  const name = e.currentTarget.item.value;
-
-  if (!name) return;
-
-  const item = {
-    id: Date.now(),
-    name: name,
-    completed: false,
-  };
-
-  items.push(item);
-
-  e.currentTarget.reset();
-
-  console.log(items);
-
-  updateItems();
-};
-
 function restoreFromLocalStorage() {
   console.info("Restoring from LS");
   // pull the items from LS
   const lsItems = JSON.parse(localStorage.getItem("items"));
-  if (lsItems && lsItems.length) {
+  if (lsItems.length) {
     items = lsItems;
     updateItems();
   }
 }
 
-restoreFromLocalStorage()
-
-todoForm.addEventListener("submit", handleSubmit);
+restoreFromLocalStorage();
